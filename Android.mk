a9765db00aa5be4432e3418b86c1ebe72e0cd588
Steve Kondik <steve@cyngn.com>
2016-10-22 23:36:49 +0530

    msm8956: Create RFS symlinks
    Change-Id: I04393a97c128d1913cd6e4b8cd08dbf756c093a6

--- Android.mk
+++ Android.mk
@@ -37,4 +37,6 @@ $(shell mkdir -p $(TARGET_OUT)/etc/firmware/wlan/prima; \
 	ln -sf /data/misc/wifi/WCNSS_qcom_cfg.ini \
 		$(TARGET_OUT)/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini)
 
+include device/xiaomi/msm8956-common/tftp.mk
+
 endif
