5a5c33d4909d3679b50da0be9108f2f091a95b4c
TheStrix <parthbhatia@lineageos.org>
2017-04-03 13:32:57 +0530

    msm8956: Use Snapdragon LLVM toolchain
    Change-Id: I4c00fc3c64bf2be113669dc83d7059daf2aa0026

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -189,6 +189,7 @@ TARGET_SYSTEM_PROP += $(VENDOR_PATH)/system.prop
 # Qualcomm
 BOARD_USES_QCOM_HARDWARE := true
 BOARD_USES_QC_TIME_SERVICES := true
+TARGET_USE_SDCLANG := true
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(VENDOR_PATH)/rootdir/etc/fstab.qcom
