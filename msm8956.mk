d9eb0f60df1d8d4432a691eb468aa44e20b62ce3
TheStrix <parthbhatia@lineageos.org>
2017-05-11 21:25:01 +0530

    msm8956: Kill Gello
    Change-Id: I3c64203167fb3989ee5a5a7e3cea4c9747d2bf18

--- msm8956.mk
+++ msm8956.mk
@@ -101,10 +101,6 @@ PRODUCT_COPY_FILES += \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:/system/etc/r_submix_audio_policy_configuration.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:/system/etc/usb_audio_policy_configuration.xml
 
-# Browser
-PRODUCT_PACKAGES += \
-    Gello
-
 # Camera
 PRODUCT_PACKAGES += \
     camera.msm8952 \
