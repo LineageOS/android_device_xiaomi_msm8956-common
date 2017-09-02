4c616f537bcad85aa1a463bed1736a35c5f360cb
TheStrix <parthbhatia@lineageos.org>
2017-09-02 20:04:07 +0530

    msm8956: Add compatiblity matrix
    Change-Id: I0187df060089ec7bd6bb36fa1bf9e5685f91304a

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -162,6 +162,7 @@ TARGET_NO_RPC := true
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(VENDOR_PATH)/manifest.xml
+DEVICE_MATRIX_FILE   := $(VENDOR_PATH)/compatibility_matrix.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_msm
